c10a319fe2d20cae3587817a695afe157ad902da
Roman Kuepper <roman.kuepper@sumcumo.com>
2016-09-16 10:02:55 +0200

    end of quickstart

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+//gives access to @Component decorator function
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello</h1>'
+})
+export class AppComponent { }
